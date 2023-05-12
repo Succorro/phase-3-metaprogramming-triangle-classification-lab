49bbbbce9c38e5316be3a56b1fd41826b01b44ed
Steven Gutierrez <stevogbmv@gmail.com>
2023-05-12 16:28:28 -0700

    commit tree f0fab42f9cbf98f68ba26619d37eb71a26557fa6

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+  attr_reader :side1, :side2, :side3
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind 
+    check_triangle
+    if side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 == side2 || side2 == side3 || side1 == side3
+      :isosceles
+    else 
+      :scalene
+    end
+  end
+
+  def sides_zero?
+    [side1, side2, side3].all?(&:positive?)
+  end
+
+  def triangle_inequality?
+    side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2
+  end
+
+  def check_triangle 
+    raise TriangleError unless sides_zero? && triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
